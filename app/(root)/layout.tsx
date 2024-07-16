a1b005d38a8f2fcd4dec2dd5f02e6589041176b3
Otabek <otabektolaganov69@gmail.com>
2024-07-16 15:14:00 +0500

    fifth commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,8 @@
 import Navbar from "@/components/shared/navbar";
 import { ChildProps } from "@/types";
 import { getServerSession } from "next-auth";
-import {authOptions} from "../api/auth/[...nextauth]/route";
 import Auth from "@/components/shared/auth";
+import { authOptions } from "@/lib/authOptions";
 
 export default async function RootLayout({ children }: ChildProps) {
   const session: any = await getServerSession(authOptions);
---
+++ lib/authOptions.ts
@@ -0,0 +1,58 @@
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDatabase } from "@/lib/mongoose";
+import User from "@/models/user-model";
+import { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connectDatabase();
+        const user = await User.findOne({ email: credentials?.email });
+        if (user) return user;
+        else return null;
+      },
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }: any) {
+      await connectDatabase();
+      const existedUser = await User.findOne({ email: session.user.email });
+      if (!existedUser) {
+        const newUser = await User.create({
+          name: session.user.name,
+          email: session.user.email,
+          image: session.user.image,
+          username: session.user.name.split(" ").join("").toLowerCase(),
+          password: Math.trunc(Math.random() * 100000000),
+          isAdmin: false,
+          comment: "",
+          rating: 0.0,
+        });
+        session.currentUser = newUser;
+      } else {
+        session.currentUser = existedUser;
+      }
+      session.user.id = token.sub;
+      return session;
+    },
+  },
+  session: { strategy: "jwt" },
+  jwt: { secret: process.env.NEXTAUTH_JWT_SECRET },
+  secret: process.env.SECRET_KEY,
+};
