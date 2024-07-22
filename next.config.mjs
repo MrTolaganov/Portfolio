1e8e1f387417285815bbc9a4d613e3736cb36c94
Otabek <otabektolaganov69@gmail.com>
2024-07-22 07:46:39 +0500

    22/07/2024 07:26

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,47 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
+  // async headers() {
+  //   return [
+  //     {
+  //       // Routes this applies to
+  //       source: "/api/(.*)",
+  //       // Headers
+  //       headers: [
+  //         // Allow for specific domains to have access or * for all
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: "*",
+  //           // DOES NOT WORK
+  //           // value: process.env.ALLOWED_ORIGIN,
+  //         },
+  //         // Allows for specific methods accepted
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET, POST, PUT, DELETE, OPTIONS",
+  //         },
+  //         // Allows for specific headers accepted (These are a few standard ones)
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "Content-Type, Authorization",
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
+  async redirects() {
     return [
+      // if the header `x-redirect-me` is present,
+      // this redirect will be applied
       {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+        source: "/:path((?!another-page$).*)",
+        has: [
           {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            type: "header",
+            key: "x-redirect-me",
           },
         ],
+        permanent: false,
+        destination: "/another-page",
       },
     ];
   },
